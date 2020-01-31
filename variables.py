cb1cf1a428bd9ba31c504f6f190d2091ff0616dc
dconn20 <damien.connolly85@gmail.com>
2020-01-31 12:19:40 +0000

    added

---
+++ variables.py
@@ -0,0 +1,29 @@
+# Damien Connolly
+# Looking at variables
+
+x = 914
+y = 3
+
+z = x + y 
+
+print (z)
+
+fac = 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+print (fac)
+
+b = True
+c = False
+
+print (c)
+
+s = "damien.connolly85@gmail.com"
+
+print (s[0:15])
+
+print (s[0:15:2])
+
+l = [1, 2, 3, 4]
+
+print (l[2])
+
